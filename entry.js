7b1143f3f85fc0f699a6582baf7e7ab4ace1aee0
vamechel1 <vamechel@edu.hse.ru>
2024-05-10 19:22:55 +0300

    first commit

---
+++ entry.js
@@ -0,0 +1,53 @@
+
+    window.reactComponents = {};
+
+    window.vueComponents = {};
+
+  
+      import React from "react";
+
+      import ReactDOM from "react-dom";
+
+
+      import ReactWrapper from '../node_modules/better-docs/lib/react-wrapper.js';
+
+      window.React = React;
+
+      window.ReactDOM = ReactDOM;
+
+      window.ReactWrapper = ReactWrapper;
+
+    
+    import './styles/reset.css';
+
+    import './styles/iframe.css';
+
+  import Component0 from '../src/components/common/checkbox/Checkbox.jsx';
+reactComponents['Checkbox'] = Component0;
+
+import Component1 from '../src/components/common/table/DefaultColumnFilter/DefaultColumnFilter.jsx';
+reactComponents['DefaultColumnFilter'] = Component1;
+
+import Component2 from '../src/components/common/downloadTable/DownloadTable.jsx';
+reactComponents['DownloadTable'] = Component2;
+
+import Component3 from '../src/components/header/Header.jsx';
+reactComponents['Header'] = Component3;
+
+import Component4 from '../src/components/common/input/Input.jsx';
+reactComponents['Input'] = Component4;
+
+import Component5 from '../src/components/common/popup/Popup.jsx';
+reactComponents['Popup'] = Component5;
+
+import Component6 from '../src/components/common/radiobuttons/RadioButtons.jsx';
+reactComponents['RadioButtons'] = Component6;
+
+import Component7 from '../src/components/common/table/Table.jsx';
+reactComponents['Table'] = Component7;
+
+import Component8 from '../src/components/common/tooltip/Tooltip.jsx';
+reactComponents['Tooltip'] = Component8;
+
+import Component9 from '../src/components/header/userDropdown/UserDropdown.jsx';
+reactComponents['UserDropdown'] = Component9;
