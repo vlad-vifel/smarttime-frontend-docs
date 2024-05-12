0ca2f8d3ec1a02ef74c98f05097c03d646758579
vamechel1 <vamechel@edu.hse.ru>
2024-05-12 22:19:59 +0300

    second commit

--- entry.js
+++ entry.js
@@ -22,32 +22,212 @@
 
     import './styles/iframe.css';
 
-  import Component0 from '../src/components/common/checkbox/Checkbox.jsx';
-reactComponents['Checkbox'] = Component0;
+  import Component0 from '../src/components/pages/schedule/editSchedule/popups/AlertBusyAuditorium.jsx';
+reactComponents['AlertBusyAuditorium'] = Component0;
 
-import Component1 from '../src/components/common/table/DefaultColumnFilter/DefaultColumnFilter.jsx';
-reactComponents['DefaultColumnFilter'] = Component1;
+import Component1 from '../src/components/pages/schedule/editSchedule/popups/AlertUnexeptableGroupTime.jsx';
+reactComponents['AlertUnexeptableGroupTime'] = Component1;
 
-import Component2 from '../src/components/common/downloadTable/DownloadTable.jsx';
-reactComponents['DownloadTable'] = Component2;
+import Component2 from '../src/components/pages/schedule/editSchedule/popups/AlertUnexeptableTeacherTime.jsx';
+reactComponents['AlertUnexeptableTeacherTime'] = Component2;
 
-import Component3 from '../src/components/header/Header.jsx';
-reactComponents['Header'] = Component3;
+import Component3 from '../src/components/pages/schedule/editSchedule/popups/AlertWrongDate.jsx';
+reactComponents['AlertWrongDate'] = Component3;
 
-import Component4 from '../src/components/common/input/Input.jsx';
-reactComponents['Input'] = Component4;
+import Component4 from '../src/App.jsx';
+reactComponents['App'] = Component4;
 
-import Component5 from '../src/components/common/popup/Popup.jsx';
-reactComponents['Popup'] = Component5;
+import Component5 from '../src/components/pages/guides/auditorium/Auditorium.jsx';
+reactComponents['Auditorium'] = Component5;
 
-import Component6 from '../src/components/common/radiobuttons/RadioButtons.jsx';
-reactComponents['RadioButtons'] = Component6;
+import Component6 from '../src/components/pages/home/Card/Card.jsx';
+reactComponents['Card'] = Component6;
 
-import Component7 from '../src/components/common/table/Table.jsx';
-reactComponents['Table'] = Component7;
+import Component7 from '../src/components/pages/polling/form/cellDropdown/CellDropdown.jsx';
+reactComponents['CellDropdown'] = Component7;
 
-import Component8 from '../src/components/common/tooltip/Tooltip.jsx';
-reactComponents['Tooltip'] = Component8;
+import Component8 from '../src/components/pages/schedule/editSchedule/features/changes/ChangeAuditorium.jsx';
+reactComponents['ChangeAuditorium'] = Component8;
 
-import Component9 from '../src/components/header/userDropdown/UserDropdown.jsx';
-reactComponents['UserDropdown'] = Component9;
+import Component9 from '../src/components/pages/schedule/editSchedule/features/changeDate/ChangeDate.jsx';
+reactComponents['ChangeDate'] = Component9;
+
+import Component10 from '../src/components/pages/schedule/editSchedule/features/changes/ChangeEndTime.jsx';
+reactComponents['ChangeEndTime'] = Component10;
+
+import Component11 from '../src/components/pages/schedule/editSchedule/features/changes/ChangeFormat.jsx';
+reactComponents['ChangeFormat'] = Component11;
+
+import Component12 from '../src/components/pages/schedule/editSchedule/features/changes/ChangeNote.jsx';
+reactComponents['ChangeNote'] = Component12;
+
+import Component13 from '../src/components/pages/schedule/editSchedule/features/changes/ChangeStartTime.jsx';
+reactComponents['ChangeStartTime'] = Component13;
+
+import Component14 from '../src/components/common/checkbox/Checkbox.jsx';
+reactComponents['Checkbox'] = Component14;
+
+import Component15 from '../src/components/pages/polling/form/restrictions/checkboxRestrictions/CheckboxRestrictions.jsx';
+reactComponents['CheckboxRestrictions'] = Component15;
+
+import Component16 from '../src/components/pages/guides/workload/checkboxWorkload/CheckboxWorkload.jsx';
+reactComponents['CheckboxWorkload'] = Component16;
+
+import Component17 from '../src/components/pages/polling/form/collapsible/Collapsible.jsx';
+reactComponents['Collapsible'] = Component17;
+
+import Component18 from '../src/components/pages/schedule/editSchedule/topbar/datePicker/DatePicker.jsx';
+reactComponents['DatePicker'] = Component18;
+
+import Component19 from '../src/components/common/table/DefaultColumnFilter/DefaultColumnFilter.jsx';
+reactComponents['DefaultColumnFilter'] = Component19;
+
+import Component20 from '../src/components/common/downloadTable/DownloadTable.jsx';
+reactComponents['DownloadTable'] = Component20;
+
+import Component21 from '../src/components/pages/schedule/editSchedule/tools/dropdown/Dropdown.jsx';
+reactComponents['Dropdown'] = Component21;
+
+import Component22 from '../src/components/pages/schedule/editSchedule/tools/dropdown/DropdownHide.jsx';
+reactComponents['DropdownHide'] = Component22;
+
+import Component23 from '../src/components/pages/schedule/editSchedule/EditSchedule.jsx';
+reactComponents['EditSchedule'] = Component23;
+
+import Component24 from '../src/components/pages/schedule/editSchedule/features/Features.jsx';
+reactComponents['Features'] = Component24;
+
+import Component25 from '../src/components/footer/Footer.jsx';
+reactComponents['Footer'] = Component25;
+
+import Component26 from '../src/components/pages/polling/form/Form.jsx';
+reactComponents['Form'] = Component26;
+
+import Component27 from '../src/components/pages/guides/Guides.jsx';
+reactComponents['Guides'] = Component27;
+
+import Component28 from '../src/components/header/Header.jsx';
+reactComponents['Header'] = Component28;
+
+import Component29 from '../src/components/pages/help/Help.jsx';
+reactComponents['Help'] = Component29;
+
+import Component30 from '../src/components/pages/guides/workload/hideColumns/HideColumns.jsx';
+reactComponents['HideColumns'] = Component30;
+
+import Component31 from '../src/components/pages/polling/results/resultsParameters/hideColumns/HideColumns.jsx';
+reactComponents['HideColumns'] = Component31;
+
+import Component32 from '../src/components/pages/polling/results/resultsRestrictions/hideColumns/HideColumns.jsx';
+reactComponents['HideColumns'] = Component32;
+
+import Component33 from '../src/components/pages/home/Home.jsx';
+reactComponents['Home'] = Component33;
+
+import Component34 from '../src/components/common/input/Input.jsx';
+reactComponents['Input'] = Component34;
+
+import Component35 from '../src/components/pages/instruction/Instruction.jsx';
+reactComponents['Instruction'] = Component35;
+
+import Component36 from '../src/components/pages/login/Login.jsx';
+reactComponents['Login'] = Component36;
+
+import Component37 from '../src/components/pages/schedule/editSchedule/planner/myEvent/MyEventComponent.jsx';
+reactComponents['MyEventComponent'] = Component37;
+
+import Component38 from '../src/components/pages/schedule/editSchedule/planner/myEventWrapper/MyEventWrapperComponentDraggable.jsx';
+reactComponents['MyEventWrapperComponentDraggable'] = Component38;
+
+import Component39 from '../src/components/pages/schedule/editSchedule/planner/myRange/MyRange.jsx';
+reactComponents['MyRange'] = Component39;
+
+import Component40 from '../src/components/pages/404/404.jsx';
+reactComponents['Page404'] = Component40;
+
+import Component41 from '../src/components/pages/polling/form/parameters/Parameters.jsx';
+reactComponents['Parameters'] = Component41;
+
+import Component42 from '../src/components/pages/schedule/editSchedule/planner/Planner.jsx';
+reactComponents['Planner'] = Component42;
+
+import Component43 from '../src/components/pages/polling/Polling.jsx';
+reactComponents['Polling'] = Component43;
+
+import Component44 from '../src/components/common/popup/Popup.jsx';
+reactComponents['Popup'] = Component44;
+
+import Component45 from '../src/components/pages/help/question/Question.jsx';
+reactComponents['Question'] = Component45;
+
+import Component46 from '../src/components/common/radiobuttons/RadioButtons.jsx';
+reactComponents['RadioButtons'] = Component46;
+
+import Component47 from '../src/components/pages/404/RedirectToLogin.jsx';
+reactComponents['RedirectToLogin'] = Component47;
+
+import Component48 from '../src/components/pages/polling/form/restrictions/Resctrictions.jsx';
+reactComponents['Restrictions'] = Component48;
+
+import Component49 from '../src/components/pages/polling/results/Results.jsx';
+reactComponents['Results'] = Component49;
+
+import Component50 from '../src/components/pages/polling/results/resultsParameters/ResultsParameters.jsx';
+reactComponents['ResultsParameters'] = Component50;
+
+import Component51 from '../src/components/pages/polling/results/resultsRestrictions/ResultsRestrictions.jsx';
+reactComponents['ResultsRestrictions'] = Component51;
+
+import Component52 from '../src/components/root/Root.jsx';
+reactComponents['Root'] = Component52;
+
+import Component53 from '../src/components/pages/schedule/Schedule.jsx';
+reactComponents['Schedule'] = Component53;
+
+import Component54 from '../src/components/pages/common/scrollablePage/ScrollablePage.jsx';
+reactComponents['ScrollablePage'] = Component54;
+
+import Component55 from '../src/components/pages/guides/workload/setDate/SetDate.jsx';
+reactComponents['SetDate'] = Component55;
+
+import Component56 from '../src/components/pages/schedule/editSchedule/topbar/settings/Settings.jsx';
+reactComponents['Settings'] = Component56;
+
+import Component57 from '../src/components/sidebar/Sidebar.jsx';
+reactComponents['Sidebar'] = Component57;
+
+import Component58 from '../src/components/pages/common/staticPage/StaticPage.jsx';
+reactComponents['StaticPage'] = Component58;
+
+import Component59 from '../src/components/common/table/Table.jsx';
+reactComponents['Table'] = Component59;
+
+import Component60 from '../src/components/pages/polling/form/teacherDropdown/TeacherDropdown.jsx';
+reactComponents['TeacherDropdown'] = Component60;
+
+import Component61 from '../src/components/pages/users/tokenPopup/TokenPopup.jsx';
+reactComponents['TokenPopup'] = Component61;
+
+import Component62 from '../src/components/pages/schedule/editSchedule/tools/Tools.jsx';
+reactComponents['Tools'] = Component62;
+
+import Component63 from '../src/components/common/tooltip/Tooltip.jsx';
+reactComponents['Tooltip'] = Component63;
+
+import Component64 from '../src/components/pages/schedule/editSchedule/topbar/Topbar.jsx';
+reactComponents['Topbar'] = Component64;
+
+import Component65 from '../src/components/pages/users/userCellDropdown/UserCellDropdown.jsx';
+reactComponents['UserCellDropdown'] = Component65;
+
+import Component66 from '../src/components/header/userDropdown/UserDropdown.jsx';
+reactComponents['UserDropdown'] = Component66;
+
+import Component67 from '../src/components/pages/users/Users.jsx';
+reactComponents['Users'] = Component67;
+
+import Component68 from '../src/components/pages/guides/workload/Workload.jsx';
+reactComponents['Workload'] = Component68;
+
+import Component69 from '../src/components/pages/guides/workloadUpload/WorkloadUpload.jsx';
+reactComponents['WorkloadUpload'] = Component69;
